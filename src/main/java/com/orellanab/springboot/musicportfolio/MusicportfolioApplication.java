0b078e4bc7e364542d8afd804cf86013dabf3cb5
Benyair Orellana <ben.orellana@gmail.com>
2019-04-21 20:46:36 -0400

    First Commit

---
+++ src/main/java/com/orellanab/springboot/musicportfolio/MusicportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.orellanab.springboot.musicportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicportfolioApplication.class, args);
+	}
+
+}
